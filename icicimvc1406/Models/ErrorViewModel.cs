506d34be384eb2076056d78cf16a9578e34cb05c
anshu <anshu@DESKTOP-9G8T4K6>
2022-06-14 12:41:33 +0530

    Add project files.

---
+++ icicimvc1406/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace icicimvc1406.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
